f6fa0ff10315c02d8a1e892be143422439eb6beb
maiquelb <maiquelb@gmail.com>
2024-08-09 08:42:02 -0300

    update simulator extension

--- resources/turtlesim-extension/energy_turtle.py
+++ resources/turtlesim-extension/energy_turtle.py
@@ -87,7 +87,9 @@ class TurtleSimNode(Node):
         global pen_turtle2        
         print("Energy turtle 1: " + str(self.energy_turtle1))
         if self.energy_turtle1 > 0:
-            self.energy_turtle1 -= random.uniform(1, energy_decrement_max)
+            min_vel = self.vel_turtle1 * 10
+            max_vel = self.vel_turtle1 * 100
+            self.energy_turtle1 -= random.uniform(min_vel, max_vel)
             self.publisher1.publish(Int32(data=int(self.energy_turtle1)))
             if self.energy_turtle1 >= 35:
                 pen_turtle1[2] = int((int(self.energy_turtle1)/10 - 35) * 255 / 65)
